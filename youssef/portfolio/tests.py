4397c18bc5c56f9c111c147abc615100d308dea7
Youssef Benkirane <youssef.benkirane@epita.fr>
2020-11-23 17:12:17 +0100

    first test

--- youssef/portfolio/tests.py
+++ youssef/portfolio/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+
+class PhotoTest(TestCase):
+
+    def setUp(self):
+        c = Client()
+        self.response = c.get('/photography')
+
+    def test_server_response(self):
+        self.assertEqual(self.response.status_code, 200)
 
 # Create your tests here.
